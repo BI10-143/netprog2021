048407a81ef164c1b5aceb04184458079b436706
BI10-143 <phuongnv.bi10-143@st.usth.edu.vn>
2021-05-04 08:00:11 +0700

    Labwork 2

---
+++ 02.practical.work.gethostbyname.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+int main(int argc, char **argv) {
+    struct hostent *dell;
+    if (argc < 2) {
+      char host[1000];
+      printf("Please enter the hostname: ");
+      scanf("%s", host)
+      dell = gethostbyname(host);
+    }
+    else {
+      dell = gethostbyname(argv[1]);
+    }
+
+    if (dell == NULL) {
+       printf("gethostbyname() failed to locate\n");
+    } else {
+       printf("%s: ", dell->h_name);
+       unsigned int i=0;
+       while ( dell -> h_addr_list[i] != NULL) {
+          printf( "%s ", inet_ntoa( *( struct in_addr*)( dell -> h_addr_list[i]))); 
+		  i++;
+       }
+       printf(".\n");
+    }
+
+}
